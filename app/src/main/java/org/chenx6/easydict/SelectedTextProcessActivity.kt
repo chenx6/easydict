c475a1117cb0e289a6d72deac1329c640256902a
chen_null <abc82766@gmail.com>
2022-01-16 12:42:52 +0800

    add more options to show the selected translation

--- app/src/main/java/org/chenx6/easydict/SelectedTextProcessActivity.kt
+++ app/src/main/java/org/chenx6/easydict/SelectedTextProcessActivity.kt
@@ -3,16 +3,24 @@ package org.chenx6.easydict
 import android.app.Activity
 import android.content.Intent
 import android.os.Bundle
+import android.widget.ImageButton
+import android.widget.TextView
 import android.widget.Toast
+import androidx.preference.PreferenceManager
 import kotlinx.coroutines.*
 
 class SelectedTextProcessActivity : Activity() {
     private val mainScope = MainScope()
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        // setContentView(R.layout.activity_selected_text_process)
+        setContentView(R.layout.activity_selected_text_process)
+        // Get result showing method
+        val prefManager = PreferenceManager.getDefaultSharedPreferences(applicationContext)
+        val processMethod = prefManager.getString("result_show", "toast")!!
+        // Get query word from intent
         val word = intent.getCharSequenceExtra(Intent.EXTRA_PROCESS_TEXT).toString()
         mainScope.launch {
+            // Query word
             val resultList = withContext(Dispatchers.IO) {
                 WordDatabase.getInstance(applicationContext).getWordDao().searchWord(word, 1)
             }
@@ -21,9 +29,22 @@ class SelectedTextProcessActivity : Activity() {
             } else {
                 resultList[0].translation?.replace("\\n", "\n") ?: "$word 缺少翻译"
             }
-            val toast = Toast.makeText(applicationContext, text, Toast.LENGTH_LONG)
-            toast.show()
-            finish()
+            // Show the result in different method
+            when (processMethod) {
+                "toast" -> {
+                    val toast = Toast.makeText(applicationContext, text, Toast.LENGTH_LONG)
+                    toast.show()
+                    finish()
+                }
+                "float" -> {
+                    val selectedWordView: TextView = findViewById(R.id.selectedWordView)
+                    val selectedTranslateView: TextView = findViewById(R.id.selectedTranslateView)
+                    val closeResultButton: ImageButton = findViewById(R.id.closeResultButton)
+                    selectedWordView.text = word
+                    selectedTranslateView.text = text
+                    closeResultButton.setOnClickListener { finish() }
+                }
+            }
         }
     }
 
